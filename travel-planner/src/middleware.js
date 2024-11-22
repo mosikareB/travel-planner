5b588d0ce3b9bd1b607f1bf47dc054ebc07613dc
P SIDHARTH <philkhanasidharth14@gmail.com>
2024-11-22 15:57:07 +0530

    middleware changed
    Signed-off-by: P SIDHARTH <philkhanasidharth14@gmail.com>

--- travel-planner/src/middleware.js
+++ travel-planner/src/middleware.js
@@ -6,9 +6,9 @@ export function middleware(request) {
   const privatePath = ["/home"];
   const isPrivate = privatePath.includes(path);
 
-  //   const token = request.cookies.get("__Secure-next-auth.session-token")?.value;
+    const token = request.cookies.get("__Secure-next-auth.session-token")?.value;
 
-  const token = request.cookies.get("next-auth.session-token")?.value;
+  // const token = request.cookies.get("next-auth.session-token")?.value;
 
   if (isPrivate && !token) {
     return NextResponse.redirect(new URL("/signup", request.nextUrl));
